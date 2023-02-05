d0364cb7406249ab9a13e2e57830a8fa52a91a82
Seddaoui Yassine <yassineseddaoui@gmail.com>
2023-02-05 08:32:00 -0500

    UPDATE FINAL

--- src/App.svelte
+++ src/App.svelte
@@ -80,6 +80,7 @@
 					height: 720 }});
 				video.srcObject = stream;
 			});
+			
 			takepicbutton.addEventListener('click', function() {
 				canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 					image = canvas.toDataURL('image/jpeg');
@@ -174,13 +175,79 @@
 </script>
 
 <main>
-	
-	{#if numPage == 0}
-	<p>Your text is: </p>
-	<p style="font-size:{currentFont}px;">{response}</p>
-	<button on:click={logTesseract}>Read Image!</button>
+	<input type="hidden" id="anPageName" name="page" value="macbook-pro-14-1" />
+    <div class="container-center-horizontal">
+      <div class="macbook-pro-14-1 screen">
+        <div class="overlap-group5">
+          <div class="overlap-group8">
+            <div class="button_capture"><div class="capture inter-bold-white-24px">Capture</div></div>
+            <div class="button_open-cam"><div class="open-cam inter-bold-white-24px">Open Cam</div></div>
+            <div class="overlap-group6">
+              <div class="bubble3"></div>
+              <div class="logo">
+                <div class="overlap-group4">
+                  <div class="paper_fill"><img class="subtract" src="img/subtract@2x.png" alt="Subtract" /></div>
+                  <h1 class="title">Reader</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex-row">
+            <div class="bubble2"></div>
+            <div class="read_button"><div on:click={logTesseract} class="read inter-bold-white-24px">Read!</div></div>
+          </div>
+          <div class="flex-row-1">
+            <div class="button_-container">
+              <div class="button">
+                <div class="overlap-group">
+                  <div class="x15 inter-bold-ocean-green-24px">x1.5</div>
+                  <div class="rectangle-403"></div>
+                  <div on:click={changeLang} class="fr inter-bold-ocean-green-22px">
+					{#if language == "fr"}
+					Fr
+					{:else}
+					En
+					{/if}
+				</div>
+                </div>
+              </div>
+              <div class="button-1 button-3">
+                <img on:click={pauseStart} class="_fill" src="img/stop-and-play-fill@2x.png" alt="Stop_and_play_fill" />
+              </div>
+              <div class="button_x">
+                <div class="overlap-group">
+                  <div class="x15-4 inter-bold-ocean-green-24px">x1.5</div>
+                  <div class="rectangle-403"></div>
+                  <div on:click={() => changeSpeed(1)} class="x1 inter-bold-ocean-green-22px">x1</div>
+                </div>
+              </div>
+              <div on:click={upFont} class="button-2 button-3"><div class="text">+</div></div>
+            </div>
+            <div class="button_-container-1">
+              <div class="button_x">
+                <div class="overlap-group">
+                  <div class="x15-4 inter-bold-ocean-green-24px">x1.5</div>
+                  <div class="rectangle-403"></div>
+                  <div on:click={() => changeSpeed(5)} class="x2 inter-bold-ocean-green-22px">x2</div>
+                </div>
+              </div>
+              <div on:click={downFont} class="button-2 button-3"><div class="text">-</div></div>
+            </div>
+            <div class="overlap-group7"><div class="your-text">
+				<p style="font-size: {currentFont}px;">{response}</p>
+				{#if isLoading == true}
+				Loading...
+				{/if}
+			</div></div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-	{:else}
+	<!--
+ 
+	<p style="font-size:{currentFont}px;">{response}</p>
+	<button on:click={logTesseract}>Read!</button>
 
 	<p>Your text is: </p>
 	<h2>
@@ -202,8 +269,7 @@
 		English
 		{/if}
 	</button>
-	<button on:click={goBack}>Go Back</button>
-	{/if}
+	<button on:click={goBack}>Go Back</button>-->
 </main>
 
 <style>
@@ -219,5 +285,449 @@
 			max-width: none;
 		}
 	}
+
+	@import url("https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css");
+/* The following line is used to measure usage of this code in production. For more info see our usage billing page */
+@import url("https://px.animaapp.com/63df1a9c241dcc1f34c43355.63df1a9cadcdfbdea71f54e9.xvombGA.hcp.png");
+
+
+.screen a {
+  display: contents;
+  text-decoration: none;
+}
+
+.container-center-horizontal {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  pointer-events: none;
+  width: 100%;
+}
+
+.container-center-horizontal > * {
+  flex-shrink: 0;
+  pointer-events: auto;
+}
+
+@import url("https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css");
+@import url("https://fonts.googleapis.com/css?family=Inter:700|Italiana:400");
+/* The following line is used to measure usage of this code in production. For more info see our usage billing page */
+@import url("https://px.animaapp.com/63df1a9c241dcc1f34c43355.63df1a9cadcdfbdea71f54e9.un8gqea.hcp.png");
+
+
+
+.screen a {
+  display: contents;
+  text-decoration: none;
+}
+
+.container-center-horizontal {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  pointer-events: none;
+  width: 100%;
+}
+
+.container-center-horizontal > * {
+  flex-shrink: 0;
+  pointer-events: auto;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+
+
+
+/* screen - macbook-pro-14-1 */
+
+.macbook-pro-14-1 {
+  align-items: flex-start;
+  background-color: #00000033;
+  border: 1px none;
+  display: flex;
+  height: 2946px;
+  width: 1512px;
+}
+
+.macbook-pro-14-1 .overlap-group5 {
+  align-items: flex-end;
+  background-color: var(--ocean-green);
+  display: flex;
+  flex-direction: column;
+  gap: 123px;
+  min-height: 2946px;
+  width: 1512px;
+}
+
+.macbook-pro-14-1 .overlap-group8 {
+  height: 900px;
+  position: relative;
+  width: 1491px;
+}
+
+.macbook-pro-14-1 .button_capture {
+  align-items: flex-start;
+  background-color: var(--tuna);
+  border-radius: 40px;
+  display: flex;
+  height: 52px;
+  left: 81px;
+  min-width: 151px;
+  padding: 11px 8px;
+  position: absolute;
+  top: 472px;
+}
+
+.macbook-pro-14-1 .capture {
+  letter-spacing: 0;
+  line-height: normal;
+  min-height: 29px;
+  text-align: center;
+  width: 135px;
+}
+
+.macbook-pro-14-1 .button_open-cam {
+  align-items: flex-start;
+  background-color: var(--tuna);
+  border-radius: 40px;
+  display: flex;
+  height: 52px;
+  left: 81px;
+  min-width: 151px;
+  padding: 11px 8px;
+  position: absolute;
+  top: 369px;
+}
+
+.macbook-pro-14-1 .open-cam {
+  letter-spacing: 0;
+  line-height: normal;
+  min-height: 29px;
+  text-align: center;
+  width: 135px;
+}
+
+.macbook-pro-14-1 .overlap-group6 {
+  height: 900px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 1491px;
+}
+
+.macbook-pro-14-1 .bubble3 {
+  background-color: var(--gallery);
+  border-radius: 102px 0px 0px 102px;
+  height: 900px;
+  left: 320px;
+  position: absolute;
+  top: 0;
+  width: 1171px;
+}
+
+.macbook-pro-14-1 .logo {
+  align-items: flex-start;
+  display: flex;
+  height: 78px;
+  left: 0;
+  min-width: 331px;
+  position: absolute;
+  top: 45px;
+}
+
+.macbook-pro-14-1 .overlap-group4 {
+  height: 79px;
+  margin-top: -1px;
+  position: relative;
+  width: 331px;
+}
+
+.macbook-pro-14-1 .paper_fill {
+  align-items: center;
+  display: flex;
+  height: 78px;
+  left: 17px;
+  min-width: 60px;
+  padding: 0 5.0px;
+  position: absolute;
+  top: 1px;
+}
+
+.macbook-pro-14-1 .subtract {
+  height: 58px;
+  width: 50px;
+}
+
+.macbook-pro-14-1 .title {
+  color: var(--white);
+  font-family: var(--font-family-italiana);
+  font-size: var(--font-size-xl);
+  font-weight: 400;
+  left: 0;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 0;
+  width: 331px;
+}
+
+.macbook-pro-14-1 .flex-row {
+  align-items: center;
+  align-self: flex-start;
+  display: flex;
+  gap: 93px;
+  min-width: 1415px;
+}
+
+.macbook-pro-14-1 .bubble2 {
+  background-color: var(--gallery);
+  border-radius: 0px 102px 102px 0px;
+  height: 900px;
+  width: 1171px;
+}
+
+.macbook-pro-14-1 .read_button {
+  align-items: flex-start;
+  background-color: var(--tuna);
+  border-radius: 40px;
+  display: flex;
+  height: 52px;
+  min-width: 151px;
+  padding: 9.3px 31.4px;
+}
+
+.macbook-pro-14-1 .read {
+  letter-spacing: 0;
+  line-height: normal;
+  min-height: 31px;
+  text-align: center;
+  width: 88px;
+}
+
+.macbook-pro-14-1 .flex-row-1 {
+  align-items: center;
+  display: flex;
+  min-width: 1421px;
+}
+
+.macbook-pro-14-1 .button_-container {
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  gap: 38px;
+  margin-bottom: 76.0px;
+  min-height: 318px;
+  width: 52px;
+}
+
+.macbook-pro-14-1 .button {
+  align-items: flex-start;
+  background-color: var(--tuna);
+  border-radius: 18px;
+  display: flex;
+  margin-left: 1px;
+  min-width: 51px;
+}
+
+.macbook-pro-14-1 .overlap-group {
+  border-radius: 18px;
+  height: 51px;
+  position: relative;
+  width: 51px;
+}
+
+.macbook-pro-14-1 .x15 {
+  left: 0;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 9px;
+  width: 51px;
+}
+
+.macbook-pro-14-1 .rectangle-403 {
+  background-color: var(--tuna);
+  border-radius: 18px;
+  height: 51px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 51px;
+}
+
+.macbook-pro-14-1 .fr {
+  left: 0;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 9px;
+  width: 51px;
+}
+
+.macbook-pro-14-1 .button-1 {
+  align-items: center;
+  margin-left: 1px;
+  padding: 0 9px;
+}
+
+.macbook-pro-14-1 ._fill {
+  height: 33px;
+  width: 33px;
+}
+
+.macbook-pro-14-1 .button_x {
+  align-items: flex-start;
+  background-color: var(--tuna);
+  border-radius: 18px;
+  display: flex;
+  min-width: 51px;
+}
+
+.macbook-pro-14-1 .x1 {
+  left: 0;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 9px;
+  width: 51px;
+}
+
+.macbook-pro-14-1 .button-2 {
+  align-items: flex-start;
+  justify-content: flex-end;
+  padding: 4px 8px;
+}
+
+.macbook-pro-14-1 .text {
+  color: var(--ocean-green);
+  font-family: var(--font-family-inter);
+  font-size: var(--font-size-l);
+  font-weight: 700;
+  letter-spacing: 0;
+  line-height: normal;
+  min-height: 35px;
+  text-align: center;
+  width: 34px;
+}
+
+.macbook-pro-14-1 .button_-container-1 {
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  gap: 38px;
+  margin-bottom: 76.0px;
+  margin-left: 33px;
+  min-height: 318px;
+  width: 52px;
+}
+
+.macbook-pro-14-1 .eng {
+  left: 0;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 9px;
+  width: 51px;
+}
+
+.macbook-pro-14-1 .x2 {
+  left: 0;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 9px;
+  width: 51px;
+}
+
+.macbook-pro-14-1 .overlap-group7 {
+  align-items: flex-end;
+  background-color: var(--gallery);
+  border-radius: 102px 0px 0px 102px;
+  display: flex;
+  height: 900px;
+  justify-content: flex-end;
+  margin-left: 113px;
+  min-width: 1171px;
+  padding: 58px 59px;
+}
+
+.macbook-pro-14-1 .your-text {
+  color: #33363fb8;
+  font-family: var(--font-family-inter);
+  font-size: var(--font-size-m);
+  font-weight: 700;
+  letter-spacing: 0;
+  line-height: normal;
+  min-height: 781px;
+  width: 1026px;
+}
+
+.macbook-pro-14-1 .button-3 {
+  background-color: var(--tuna);
+  border-radius: 18px;
+  display: flex;
+  height: 51px;
+  min-width: 51px;
+  
+}
+
+.macbook-pro-14-1 .x15-4 {
+  left: 0;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 9px;
+  width: 51px;
+}
+
+:root { 
+  --black: #000000;
+  --gallery: #eeeded;
+  --ocean-green: #34b175;
+  --tuna: #33363f;
+  --white: #ffffff;
+ 
+  --font-size-l: 30px;
+  --font-size-m: 24px;
+  --font-size-s: 22px;
+  --font-size-xl: 48px;
+ 
+  --font-family-inter: "Inter", Helvetica;
+  --font-family-italiana: "Italiana", Helvetica;
+}
+.inter-bold-ocean-green-24px {
+  color: var(--ocean-green);
+  font-family: var(--font-family-inter);
+  font-size: var(--font-size-m);
+  font-style: normal;
+  font-weight: 700;
+}
+
+.inter-bold-ocean-green-22px {
+  color: var(--ocean-green);
+  font-family: var(--font-family-inter);
+  font-size: var(--font-size-s);
+  font-style: normal;
+  font-weight: 700;
+}
+
+.inter-bold-white-24px {
+  color: var(--white);
+  font-family: var(--font-family-inter);
+  font-size: var(--font-size-m);
+  font-style: normal;
+  font-weight: 700;
+}
+
 
 </style>
